4cb08d5275fb4801c391b408701cf2f44b635eef
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-02-04 19:39:24 +0100

    Update homework2.py
    homework2 corrected

--- homework2.py
+++ homework2.py
@@ -1,9 +1,15 @@
 from random import randint
-def get_numbers_ticket(min, max, quantity):
+
+def get_numbers_ticket(min_val, max_val, quantity):
+    if not (1 <= min_val <= max_val <= 1000) or not (1 <= quantity <= (max_val - min_val + 1)):
+        return []
+
     result_array = set()
-    while len(result_array) != quantity:
-        result_array.add(randint(min,max))
-        numbers = sorted(list(result_array))
-    return numbers
-lottery_numbers = get_numbers_ticket(1,55, 3)
+    while len(result_array) < quantity:
+        result_array.add(randint(min_val, max_val))
+
+    return sorted(result_array)
+
+
+lottery_numbers = get_numbers_ticket(10,14, 6)
 print("Your lottery numbers:", lottery_numbers)
