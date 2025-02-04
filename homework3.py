a8720db602a81801dd7d5c79858cd2c0a0c73297
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-02-04 20:03:47 +0100

    Update homework3.py
    homework3 corrected

--- homework3.py
+++ homework3.py
@@ -1,18 +1,23 @@
 import re
-phone_numbers = [
-"    +38(050)123-32-34",
-"     0503451234",
-"(050)8889900",
-"38050-111-22-22",
-"38050 111 22 11   "
 
-]
+def normalize_phone(phone_number):
+    cleaned_number = re.sub(r'\D', '', phone_number)
 
-for phone in phone_numbers:
-    cleaned_number = re.sub(r'\D', '', phone)
     if not cleaned_number.startswith('+'):
         if cleaned_number.startswith('380'):
             cleaned_number = '+' + cleaned_number
         else:
             cleaned_number = '+38' + cleaned_number
-    print(cleaned_number)
+
+    return cleaned_number
+
+raw_numbers = [
+"    +38(050)123-32-34",
+"     0503451234",
+"(050)8889900",
+"38050-111-22-22",
+"38050 111 22 11   "
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
