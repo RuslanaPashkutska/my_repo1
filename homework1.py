f47606331df0b8a596b278c0fe3b7290fda26f6f
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-02-04 19:12:09 +0100

    Update homework1.py
    homework1 corrected

--- homework1.py
+++ homework1.py
@@ -6,16 +6,16 @@ def get_days_from_today(date_str):
     try:
         given_date = datetime.strptime(date_str, "%Y-%m-%d").date()
     except ValueError:
-        raise ValueError ("Error!!!  Expected format YYYY-MM-DD")
+        return "Error!!!  Expected format YYYY-MM-DD"
 
     current_date = datetime.today().date()
     delta = current_date - given_date
 
     return delta.days
 
+
 if __name__ == "__main__":
-    try:
-        days_difference = get_days_from_today("2027-01-02")
-        print("Returned difference in days:", days_difference)
-    except ValueError as error:
-        print("Error:", error)
+   test_date = ["2019-08-23", "2026-09-05", "hello", "2023-16-02", "not-correct"]
+
+   for test in test_date:
+       print (f"Datos: {test} Resultado: {get_days_from_today(test)}")
